28de6330b2f52a6e9ca2ec5eb49440f6dfd6a218
mrveavc <mrveavc12@gmail.com>
2024-01-22 00:22:40 +0300

    Proje dosyası ekle.

---
+++ Example/Program.cs
@@ -0,0 +1,100 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json.Serialization;
+using Example;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+var personalUsers = JsonConvert.DeserializeObject<IList<Personal>>(Datas.PersonalJson);
+var studentUsers = JsonConvert.DeserializeObject<IList<Student>>(Datas.StudentJson) ;
+var jobberUsers = JsonConvert.DeserializeObject<IList<Jobber>>(Datas.JobberJson);
+IDictionary<string,IList<string>> indexes =new Dictionary<string,IList<string>>();
+IDictionary<string, IUser> fastList = new Dictionary<string, IUser>();
+
+fastList.AddToDictionary(studentUsers.Select(user=>(user as IUser)).ToList(),indexes);
+
+//var allUsers = FindByIndex("Benjamen");
+var allUsers = FindByIndex("Arnal");
+
+allUsers.ToList().ForEach(user => Console.WriteLine("\n" + JsonConvert.SerializeObject(user)));
+Console.ReadKey();
+
+IList<IUser>? FindByIndex(string search)
+{
+    if (indexes.ContainsKey(search))
+    {
+        var findedKeys=indexes[search];
+        return findedKeys.Select(key => fastList[key]).ToList();
+    }
+    return null;
+}
+public static class MicrosoftExtensions
+{
+   
+    public static List<T> FindAll<T>(this IList<T> values, Predicate<T> predicate)
+    {
+        return values.ToList().FindAll(predicate);
+    }
+ 
+   
+    public static void AddToDictionary<TKey,TValue>(
+        this IDictionary<TKey, TValue> values, 
+        IList<TValue> users,
+        IDictionary<TKey,IList<TKey>> indexes
+        )
+        where TValue: IUser
+        where TKey: notnull
+    {
+        TKey castToKey(object key)
+        {
+            return (TKey)key;
+        };
+        void addIndex(object findKeyObject, TKey dataKey)
+        {
+            TKey findKey= castToKey(findKeyObject);
+            if (indexes.ContainsKey(findKey))
+            {
+                indexes[findKey].Add(dataKey);
+            }
+            else
+            {
+                indexes.Add(findKey, new List<TKey>() { dataKey });
+            }
+        };
+        users?.ToList().ForEach(user =>
+        {
+            TKey key = castToKey(user.UserName);
+            values.Add(key, user);
+            addIndex(user.FirstName, key);
+            addIndex(user.LastName, key);
+            addIndex(user.Id, key);
+           
+            var personal = user.CastTo<IPersonal>();
+            if (personal!=null){
+                addIndex(personal.SSN, key);
+                addIndex(personal.Salary.ToString(),key);
+            }
+            var student = user.CastTo<IStudent>();
+            if (student != null)
+            {
+                addIndex(student.Absenteeism.ToString(), key);
+                addIndex(student.StudentNo, key);
+                addIndex(student.Marks.ToString(), key);
+            }
+            var jobber = user.CastTo<IJobber>();
+            if (jobber != null)
+            {
+                addIndex(jobber.PlateNo, key);
+                addIndex(jobber.WorkArea, key);
+            }
+        });
+    }
+    public static TObject? CastTo<TObject>(this object value)
+        where TObject: class
+    {
+        if (value is TObject)
+        {
+            return value as TObject;
+        }
+        return null;
+    }
+}
---
+++ Example/User.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+
+namespace Example;
+public interface IUser
+{
+    string Id { get; set; }
+    string UserName { get; set; }
+    string Password { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
+    bool IsActive { get; set; }
+}
+public interface IPersonal : IUser
+{
+    
+    string SSN { get; set; }
+    decimal Salary { get; set; }
+    void CalculateSalary(short workingHours);
+}
+
+public interface IStudent : IUser
+{
+    string StudentNo { get; set; }
+    int Absenteeism { get; set; }
+    byte Marks { get; set; }
+}
+public interface IJobber:IUser
+{
+    string PlateNo { get; set; }    
+    string WorkArea { get; set; }
+}
+
+public abstract class User:IUser
+{
+    public string Id { get; set; }  
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public bool IsActive { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public User() { }
+    public User(string userName, string password,bool isActive) {
+        UserName = userName;
+        Password = password;
+        IsActive = isActive;
+    }
+
+}
+
+public class Personal : User, IPersonal
+{
+    public string SSN { get; set; }
+    public decimal Salary { get; set; }
+    public Personal() { }
+    public Personal(string userName, string password, bool isActive, string ssn, decimal salary) : base(userName, password, isActive)
+    {
+        SSN = ssn;
+        Salary = salary;
+    }
+    public void CalculateSalary(short workingHours)
+    {
+        Console.WriteLine(@$"Calculated salary: {Salary * workingHours} ₺");
+    }
+}
+
+
+public class Student:User,IStudent {
+    
+    public string StudentNo {  get; set; }
+    public int Absenteeism { get; set; }
+    public byte Marks { get; set; }
+    public Student() { }
+    public Student(string userName, string password, bool isActive, int absenteeism, byte marks,string studentNo) : base(userName, password, isActive)
+    {
+        Absenteeism = absenteeism;
+        Marks = marks;
+        StudentNo = studentNo;
+
+    }
+}
+public class Jobber:User,IJobber
+{
+    public string PlateNo { get; set; }
+    public string WorkArea { get; set; }
+    public Jobber() { }
+    public Jobber(string userName, string password, bool isActive, string workArea,string plateNo) : base(userName, password, isActive)
+    {
+        WorkArea = workArea;
+        PlateNo = plateNo;
+
+    }
+}
